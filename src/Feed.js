6b595513c3f8d9c68bb3643e961fb00641199470
nasuka7 <n4suka7@gmail.com>
2021-03-03 23:47:53 +0900

    second commit

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "./Box"
 import Post from "./Post"
 import "./Feed.css";
+import db from "./firebase";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot => (
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, [])
+
   return (
     <div className="feed">
       <div className="feed_header">
@@ -11,12 +20,19 @@ const Feed = () => {
         <h2>ホーム</h2>
       </div>
 
-      {/* {Box} */}
-      <Box />        
+      <Box />
 
-      {/* {Post} */}
-      <Post />
-      
+      {posts.map(post => (
+        <Post 
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
+        />
+      ))}
+
     </div>
   );
 }
